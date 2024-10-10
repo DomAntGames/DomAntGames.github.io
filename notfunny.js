0ad7bd9c564715f54bcd1af61ff6855bbc1e1469
DomAntGames <DomesticatedAntGames@gmail.com>
2024-10-10 17:43:48 +0300

    More proper style sheet, all pages defined in rough form, smaller logo, some unfunny comments, doubtfully.

---
+++ notfunny.js
@@ -0,0 +1,16 @@
+//This really isn't that funny.
+let c = {};
+let n = 0;
+c[n++]= "Web design is not our passion.";
+c[n++]= "Thank you for reading?";
+c[n++]= "Probably.";
+c[n++]= "Yes.";
+c[n++]= "Close the browser tab on your way out.";
+c[n++]= "So it goes.";
+c[n++]= "Oh.";
+c[n++]= "Sorry for the trouble.";
+c[n++]= "Zzz...";
+c[n++]= "Sorry for the trouble.";
+c[n++]= "Maybe next time.";
+let num=Math.floor(Math.random() * n); 
+document.write( "<small>\""+c[num]+"\"</small>" );
